b2d96bd448573fb4cbbb47c9901ffc6ebd006189
Pabolt84 <46903625+Pabolt84@users.noreply.github.com>
2022-05-12 17:47:35 +0200

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -98,11 +98,47 @@ float x0 = mpu.getMagX();
 float y0 = mpu.getMagY();
 float TotAngle;
 
+void print_calibration() {
+    Serial.println("< calibration parameters >");
+    Serial.println("accel bias [g]: ");
+    Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+    Serial.println();
+    Serial.println("gyro bias [deg/s]: ");
+    Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.print(", ");
+    Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+    Serial.println();
+    Serial.println("mag bias [mG]: ");
+    Serial.print(mpu.getMagBiasX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagBiasZ());
+    Serial.println();
+    Serial.println("mag scale []: ");
+    Serial.print(mpu.getMagScaleX());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleY());
+    Serial.print(", ");
+    Serial.print(mpu.getMagScaleZ());
+    Serial.println();
+}
+
 void setup() {
   MiCServo.Setup();
   Serial.begin(9600);
   
   Wire.begin();
+    delay(2000);
+
+     Serial.begin(115200);
+    Wire.begin();
     delay(2000);
 
     if (!mpu.setup(0x68)) {  // change to your own address
@@ -111,6 +147,23 @@ void setup() {
             delay(5000);
         }
     }
+
+    // calibrate anytime you want to
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
+
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
+
+    print_calibration();
+
+    mpu.verbose(false);
+    
   loadCalibration();
   Serial.println("Programa no explota");
   MiCServo.MoverServo(20);
@@ -121,7 +174,22 @@ void setup() {
 }
 
 
+void print_yaw() {
+    Serial.print("Yaw: ");
+    Serial.println(mpu.getYaw(), 2);
+}
+
+
+
 void loop(){
+  
+   if (mpu.update()) {
+        static uint32_t prev_ms = millis();
+            if (millis() > prev_ms + 25) {
+                print_yaw();
+                prev_ms = millis();
+        }
+    }
 
 }
 
