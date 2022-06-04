49376f482cff2f3743926cbd37551b4ded7a0541
qu4Vix <javgon.ism@gmail.com>
2022-06-04 09:41:22 +0200

    Ultimos cambios
    Subidos ficheros

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -2,6 +2,9 @@
 #include <Servo.h>
 #include "MPU9250.h"
 #include "eeprom_utils.h"
+#include <Pixy2.h>
+#include <Adafruit_NeoPixel.h>
+#include <Ultrasonic.h>
 
 #define servoMAX 67
 #define servoMIN 13
@@ -9,12 +12,13 @@
 #define PinEnMotor 5
 #define PinDir1Motor 6
 #define PinDir2Motor 7
-#define interruptPin 2
+#define PinEncoder 2
+#define PinLED 11
+#define PinTriggerI 8
+#define PinEchoI 9
 #define kp 3
 
 long encoder = 0;
-long prevEncoder = 0;
-int incremento_Encoder = 0;
 bool forward = true;
 int vuelta = 1;
 float valor = 0;
@@ -22,7 +26,7 @@ float offset;
 float velocidad;
 long prev_tiempo = 0;
 int incremento_Tiempo = 0;
-
+float face = 0;
 
 class CServo{  //maneja el servo
 public:
@@ -53,6 +57,7 @@ class Motor{
   void potencia(int pot);
   void errorPotencia(float bearing, float target);
   int GetPotencia();
+  void arrancar();
 
   private:
   byte _pinEn;
@@ -61,21 +66,19 @@ class Motor{
   float _potencia = 0;
 
 };
-int Motor::GetPotencia(){
-  return _potencia;
-}
 
-Motor::Motor(byte PinEn,byte PinDir1,byte PinDir2){
+Motor::Motor(byte PinEn,byte PinDir1,byte PinDir2){ // setup del motor
   _pinEn = PinEn;
   _pinDir1 = PinDir1;
   _pinDir2 = PinDir2;
   pinMode(PinEn, OUTPUT);
   pinMode(PinDir1, OUTPUT);
   pinMode(PinDir2, OUTPUT);
-  // falta algo?
+
 }
 
-void encoderISR() {
+
+void encoderISR() {  // función para que funcien el encoder
   if (forward == true) 
   {
     encoder ++;
@@ -113,11 +116,51 @@ void Motor::errorPotencia(float velocidad, float target){
   potencia((int)_potencia);
 }
 
+void Motor::arrancar() {  // función para arrancar el motor
+  potencia(200);
+  if ( encoder >= 200 )
+  {
+    potencia(140);
+  }
+  
+}
+
+class Ultrasonido{
+  public:
+  Ultrasonido(byte Trigger, byte Echo);
+  long getDistancia();
+
+  private:
+  long _distancia = 0;
+  byte _Trigger;
+  byte _Echo;
+};
+
+Ultrasonido::Ultrasonido(byte Trigger, byte Echo){
+  _Trigger = Trigger;
+  _Echo = Echo;
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+}
+
+long Ultrasonido::getDistancia(){
+  return _distancia;
+}
 
 
-CServo MiCServo(3);
-Motor MiMotor(5,6,7);
+
+CServo MiCServo(PinConServo);
+Motor MiMotor(PinEnMotor,PinDir1Motor,PinDir2Motor);
 MPU9250 mpu;
+Pixy2 pixy;
+Ultrasonic UltrasonidoI(PinTriggerI, PinEchoI);
+
+void autoTurn(){
+  if (UltrasonidoI.read(CM)>60){
+    MiCServo.MoverServo(90);
+    face = face +90;
+  };
+};
 
 int ErrorDireccion(int bearing, int target){
   int error = bearing - target;
@@ -127,8 +170,8 @@ int ErrorDireccion(int bearing, int target){
   return -1*error;
 }
 
-void Calibrar(){
-  mpu.verbose(true);
+void Calibrar(){ // función para calibrar ( revisar )
+  mpu.verbose(true);  
   delay(1000);
   mpu.calibrateMag();
   mpu.calibrateAccelGyro();
@@ -137,12 +180,12 @@ void Calibrar(){
 }
 
 void setup() {
-  encoder = 0;
   MiCServo.Setup();
-  pinMode(interruptPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), encoderISR, CHANGE);
+  pinMode(PinEncoder, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PinEncoder), encoderISR, CHANGE);
   Serial.begin(115200);
-  MiCServo.MoverServo(ErrorDireccion(valor,0));
+  pixy.init();
+
   //Calibrar();
 
 
@@ -170,25 +213,34 @@ void setup() {
   }
   offset = tot/num;
   Serial.println(offset);
+  MiCServo.MoverServo(ErrorDireccion(valor,0));
   delay(100);
-  //MiMotor.potencia(150);
+  MiMotor.arrancar();
   
 }
-
-void print_yaw_gyroz() {
-    Serial.print("Yaw, GyroZ: ");
-    Serial.print(mpu.getYaw(), 2);
-    Serial.print(", ");
-    Serial.println(mpu.getGyroZ(), 2);
-}
-
-
-
 
 
 
 uint32_t Duracion_de_la_muestra = 0;
-
+/*
+void loop()
+{ 
+  int i; 
+  pixy.ccc.getBlocks();
+  
+  if (pixy.ccc.numBlocks)
+  {
+    Serial.print("Detected ");
+    Serial.println(pixy.ccc.numBlocks);
+    for (i=0; i<pixy.ccc.numBlocks; i++)
+    {
+      Serial.print("  block ");
+      Serial.print(i);
+      Serial.print(": ");
+      pixy.ccc.blocks[i].print();
+    }
+  }  
+}*/
 
 void loop() {
   static uint32_t prev_ms = millis();
@@ -197,49 +249,56 @@ void loop() {
         prev_ms = millis();
         valor = valor + ((mpu.getGyroZ() - offset)*Duracion_de_la_muestra/1000);
     }
-/*
-    if(encoder > 1500){
-//acordarse de multiplicar el 1500 por vuelta para evitar cualquier fallo
+    MiCServo.MoverServo(ErrorDireccion(valor,face));
+    /*if(encoder > 1500){
       MiCServo.MoverServo(ErrorDireccion(valor,-90*vuelta));
       if(abs(ErrorDireccion(valor,-90*vuelta))<5){
         vuelta ++;
         encoder = 0;
       }
-
-    }
-
+    }*/
+    /*
     Serial.println(encoder);
     Serial.println(vuelta);
     Serial.println(valor);
-*/  
-    static uint32_t prev_ms2 = millis();
-            if (millis() > prev_ms2 + 1) {
-              incremento_Encoder = encoder - prevEncoder;
-              incremento_Tiempo = millis() - prev_tiempo;
-              velocidad = (float)incremento_Encoder/(float)incremento_Tiempo;
-              prevEncoder = encoder;
-              prev_ms2 = millis();
-              prev_tiempo = millis();
-        }
-       
+    */
+ 
+         
     static uint32_t prev_ms3 = millis();
-            if (millis() > prev_ms3 + 200) {
+            if (millis() > prev_ms3 + 10) {
                //Serial.println(valor);
                //Serial.println(Duracion_de_la_muestra);
-              //Serial.print(velocidad);
-              /*Serial.print(",");
-              Serial.print(incremento_Encoder);
-              Serial.print(",");
-              Serial.println(incremento_Tiempo);*/
-              Serial.println(MiMotor.GetPotencia());
-              prev_ms3 = millis();
+               autoTurn();
+                prev_ms3 = millis();
         }
-    static uint32_t prev_ms4 = millis();
-            if (millis() > prev_ms4 + 5) {
-              MiMotor.errorPotencia(velocidad, 2);
-              prev_ms4 = millis();
-        }
-
- 
     
 }
+
+//para probar con los sensores de ultrasonido
+/*
+const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 3;   //Pin digital 3 para el Echo del sensor
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+}
+void loop()
+{
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+}
+*/
