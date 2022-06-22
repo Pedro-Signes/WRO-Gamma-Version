778bb8a4c661f5499fe10cfc5f7e415ed6fa49db
Pepe <47985675+Pedro-Signes@users.noreply.github.com>
2022-06-22 11:33:35 +0200

    Actualizar código de prueba

--- Pruebas ESP32/Prueba Camara/src/main.cpp
+++ Pruebas ESP32/Prueba Camara/src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <Pixy2.h>
 
+int Color1;
+int PosicionX1;
+int PosicionY1;
+int Altura1;
+int Anchura1;
+
+
 
 void setup() {
   // put your setup code here, to run once:
