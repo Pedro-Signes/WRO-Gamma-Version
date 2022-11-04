218ffa6d0f7f282e99f620072933e314b084b42b
qu4Vix <javgon.ism@gmail.com>
2022-11-04 13:19:04 +0100

    04/11/2022 V1
    Color detection test

--- code/Componentes/Color_Detector/Color_Detector.ino
+++ code/Componentes/Color_Detector/Color_Detector.ino
@@ -17,8 +17,8 @@ void setup()
 void TCS()
  {
  flag=0;
- digitalWrite(s1,HIGH);
  digitalWrite(s0,HIGH);
+ digitalWrite(s1,HIGH);
  digitalWrite(s2,LOW);
  digitalWrite(s3,LOW);
  attachInterrupt(0, ISR_INTO, CHANGE);
@@ -39,35 +39,46 @@ void ISR_INTO()
  int i=0;
  ISR(TIMER2_OVF_vect)//the timer 2, 10ms interrupt overflow again. Internal overflow interrupt executive function
 {
+    static uint32_t prev_ms = millis();
     TCNT2=100;
     flag++;
  if(flag==1)
   {
-    countR=counter;
+    countR = counter;
+    Serial.print("Muestreo: ");
+    uint32_t Muestreo = millis() - prev_ms;
+    Serial.print(Muestreo);
+    Serial.print("\t");
+    if (countR < 50) {
+      Serial.print("BLUE");
+      Serial.print("\t");
+      }
     Serial.print("red=");
     Serial.println(countR,DEC);
-    digitalWrite(s2,HIGH);
-    digitalWrite(s3,HIGH);
-  }
-  else if(flag==2)
-   {
-    countG=counter;
-    Serial.print("green=");
-    Serial.println(countG,DEC);
+    prev_ms = millis();
     digitalWrite(s2,LOW);
     digitalWrite(s3,HIGH);
-   }
-   else if(flag==3)
+  }
+  
+   else if(flag==2)
     {
     countB=counter;
+    Serial.print("Muestreo: ");
+    uint32_t Muestreo = millis() - prev_ms;
+    Serial.print(Muestreo);
+    Serial.print("\t");
+    if (countB < 50) {
+      Serial.print("RED");
+      Serial.print("\t");
+      }
     Serial.print("blue=");
     Serial.println(countB,DEC);
     Serial.println("\n");
+    prev_ms = millis();
     digitalWrite(s2,LOW);
     digitalWrite(s3,LOW);
-
     }
-    else if(flag==4)
+    else if(flag==3)
      {
      flag=0;
      }
