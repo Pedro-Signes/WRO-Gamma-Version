5b49c98b3f03878789fc7ff54439fdab2e6b16c8
qu4Vix <javgon.ism@gmail.com>
2022-11-08 18:29:06 +0100

    Nano Slave 2
    Nuevo Codigo para el Arduino Nano Every (como Slave).
    Medicion de distancia por TOF.

--- code/Componentes/SemiLaser_Distance_Test/SemiLaser_Distance_Test.ino
+++ code/Componentes/SemiLaser_Distance_Test/SemiLaser_Distance_Test.ino
@@ -41,38 +41,3 @@ void loop()
   Serial.println(" ms");
   prev_ms = millis();
 }
-/*
-
-#define sensorPin = 3;
-
-void setup()
-{
-  Serial.begin(115200);
-}
- 
-void loop()
-{
-  int16_t t = pulseIn(sensorPin, HIGH);
-
-  if (t == 0)
-  {
-    // pulseIn() did not detect the start of a pulse within 1 second.
-    Serial.println("timeout");
-  }
-  else if (t > 1850)
-  {
-    // No detection.
-    Serial.println(-1);
-  }
-  else
-  {
-    // Valid pulse width reading. Convert pulse width in microseconds to distance in millimeters.
-    int16_t d = (t - 1000) * 2;
- 
-    // Limit minimum distance to 0.
-    if (d < 0) { d = 0; } 
-  
-    Serial.print(d);
-    Serial.println(" mm");
-  }
-}*/
---
+++ code/Fase 1 - Vueltas/Nano Slave 2/src/main.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define PinLaserF 3
+#define PinLaserD 4
+#define PinLaserI 5
+#define PinColorOut 6
+#define PinColorS0 7
+#define PinColorS1 8
+#define PinColorS2 9
+#define PinColorS3 10
+
+bool ReadingLaserF = false;   // Indica si se esta leyendo el Laser Frontal
+bool ReadingLaserD = false;   // Indica si se esta leyendo el laser Derecho
+bool ReadingLaserI = false;   // Indica si se esta leyendo el laser Izquierdo
+int outputLaserF; // micros
+int outputLaserD; // micros
+int outputLaserI; // micros
+volatile uint32_t prev_mcs_laserF;
+volatile uint32_t prev_mcs_laserD;
+volatile uint32_t prev_mcs_laserI;
+
+void receiveEvent(int howMany);
+void requestEvent();
+
+void medirLaserF() {
+  uint32_t mcs = micros();
+  if (digitalRead(PinLaserF)) {
+    prev_mcs_laserF = mcs;
+    ReadingLaserF = true;
+  } else if (ReadingLaserF) {
+    outputLaserF = mcs - prev_mcs_laserF;
+    ReadingLaserF = false;
+  }
+}
+void medirLaserD() {
+  uint32_t mcs = micros();
+  if (digitalRead(PinLaserD)) {
+    prev_mcs_laserD = mcs;
+    ReadingLaserD = true;
+  } else if (ReadingLaserD) {
+    outputLaserD = mcs - prev_mcs_laserD;
+    ReadingLaserD = false;
+  }
+}
+void medirLaserI() {
+  uint32_t mcs = micros();
+  if (digitalRead(PinLaserI)) {
+    prev_mcs_laserI = mcs;
+    ReadingLaserI = true;
+  } else if (ReadingLaserI) {
+    outputLaserI = mcs - prev_mcs_laserI;
+    ReadingLaserI = false;
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  Serial.println("Serial a punto");
+
+  Wire.begin(5);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
+
+  pinMode(PinLaserF, OUTPUT);
+  pinMode(PinLaserD, OUTPUT);
+  pinMode(PinLaserI, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(PinLaserF), medirLaserF, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PinLaserD), medirLaserD, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PinLaserI), medirLaserI, CHANGE);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
