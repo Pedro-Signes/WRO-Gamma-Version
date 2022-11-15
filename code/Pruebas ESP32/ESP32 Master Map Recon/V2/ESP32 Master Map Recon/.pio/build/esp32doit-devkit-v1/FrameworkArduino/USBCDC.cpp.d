ba922f55e7b46bdc54dba3f417ce4ed4e675aea9
qu4Vix <javgon.ism@gmail.com>
2022-11-15 10:03:30 +0100

    Folders update

---
+++ code/Pruebas ESP32/ESP32 Master Map Recon/V2/ESP32 Master Map Recon/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBCDC.cpp.o: \
+ /home/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ /home/pedro/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/pedro/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qspi_qspi/include/sdkconfig.h
